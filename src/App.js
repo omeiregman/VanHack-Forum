376a5cf5605cc3a953ce3dbb8b9917aec2879b65
= <=>
2018-02-22 20:45:18 +0100

    Basic Post Setup

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="">
-        <h1>Hello VanHack</h1>
+      <div>
+        <div className="card post-body">
+          <div className="card-body">
+            Hello I am a Post
+          </div>
+        </div>
+        <div className="card post-editor">
+          <div className="card-body">
+            <textarea className="form-control post-editor-input"/>
+            <button className="btn btn-success post-editor-btn">Post</button>
+          </div>
+        </div>
       </div>
     );
   }
